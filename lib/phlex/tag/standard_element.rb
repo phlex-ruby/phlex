6c92b4c65f41cd97ec650606b8673d163a613755
Joel Drapper <joel@drapper.me>
2022-06-27 23:23:33 +0100

    Remove unused method_missing

--- lib/phlex/tag/standard_element.rb
+++ lib/phlex/tag/standard_element.rb
@@ -6,10 +6,6 @@ module Phlex
       def call
         "<#{opening_tag_content}>#{super}</#{self.class.tag_name}>"
       end
-
-      def method_missing(*args, **kwargs, &block)
-        super.tap { Context.new(self).call(&block) if block_given? }
-      end
     end
   end
 end
