ff43d8bb1686ee44a3320b7350476909439319ae
Joel Drapper <joel@drapper.me>
2022-08-18 11:48:11 +0100

    Support Ruby 2.7

--- lib/phlex.rb
+++ lib/phlex.rb
@@ -7,6 +7,16 @@ loader = Zeitwerk::Loader.for_gem
 loader.ignore("#{__dir__}/generators")
 loader.setup
 
+if Gem::Version.new(RUBY_VERSION) < Gem::Version.new("3.0")
+  unless Symbol.instance_methods.include?(:name)
+    class Symbol
+      def name
+        to_s.freeze
+      end
+    end
+  end
+end
+
 module Phlex
   Error = Module.new
   ArgumentError = Class.new(ArgumentError) { include Error }
--- phlex.gemspec
+++ phlex.gemspec
@@ -12,7 +12,7 @@ Gem::Specification.new do |spec|
   spec.description = "Better view components"
   spec.homepage = "https://github.com/joeldrapper/phlex"
   spec.license = "MIT"
-  spec.required_ruby_version = ">= 3.0"
+  spec.required_ruby_version = ">= 2.7"
 
   spec.metadata["homepage_uri"] = spec.homepage
   spec.metadata["source_code_uri"] = "https://github.com/joeldrapper/phlex"
