7b8163272e6ab20d77c55dd2e4c91b535d7997d2
Joel Drapper <joel@drapper.me>
2022-09-11 09:36:42 +0100

    Support Rails form_with helper

---
+++ lib/phlex/buffered.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Phlex
+  class Buffered
+    def initialize(object, buffer:)
+      @object, @buffer = object, buffer
+    end
+
+    def method_missing(name, *args, **kwargs, &block)
+      output = @object.public_send(name, *args, **kwargs, &block)
+      @buffer << output if output.is_a? String
+      nil
+    end
+
+    def respond_to_missing?(name)
+      @object.respond_to?(name)
+    end
+  end
+end
