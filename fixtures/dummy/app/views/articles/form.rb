503dc989ff5d60465ca2858782f7272668000338
Joel Drapper <joel@drapper.me>
2022-09-15 16:52:09 +0100

    Add very basic form test

---
+++ fixtures/dummy/app/views/articles/form.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Views
+  module Articles
+    class Form < Phlex::Component
+      def template
+        form_with url: "test" do |f|
+          f.text_field :name
+        end
+      end
+    end
+  end
+end
