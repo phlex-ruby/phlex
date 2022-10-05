95c813dfe3d4f2112b38476fb3c6b05159014975
Joel Drapper <joel@drapper.me>
2022-10-05 13:28:57 +0100

    Improve abstract table collection (#223)
    - Aliases for overrides
    - Ability to set header / column attributes

--- lib/phlex/table.rb
+++ lib/phlex/table.rb
@@ -5,15 +5,17 @@ module Phlex
 		include Collection
 
 		module ClassMethods
-			def property(header, &block)
-				if header.is_a?(String)
-					header_text = header
-					header = -> { text header_text }
+			def property(header_content, header: {}, cell: {}, &cell_content)
+				if header_content.is_a?(String)
+					header_content_text = header_content
+					header_content = -> { text header_content_text }
 				end
 
 				properties << {
-					header: header,
-					body: block
+					header_content: header_content,
+					header_attributes: header,
+					cell_content: cell_content,
+					cell_attributes: cell,
 				}
 			end
 
@@ -23,30 +25,38 @@ module Phlex
 		end
 
 		def self.included(child)
-			child.extend(ClassMethods)
+			child.extend ClassMethods
+
+			child.alias_method :table_template, :table
+			child.alias_method :head_template, :thead
+			child.alias_method :header_template, :th
+			child.alias_method :body_template, :tbody
+			child.alias_method :row_template, :tr
+			child.alias_method :head_row_template, :row_template
+			child.alias_method :cell_template, :td
 		end
 
 		def collection_template
-			table do
-				thead do
-					tr do
+			table_template do
+				head_template do
+					head_row_template do
 						self.class.properties.each do |property|
-							th scope: "col" do
-								instance_exec(&property[:header])
+							header_template(**property[:header_attributes], scope: "col") do
+								instance_exec(&property[:header_content])
 							end
 						end
 					end
 				end
 
-				tbody { yield_items }
+				body_template { yield_items }
 			end
 		end
 
 		def item_template
-			tr do
+			row_template do
 				self.class.properties.each do |property|
-					td do
-						instance_exec(@item, &property[:body])
+					cell_template(**property[:cell_attributes]) do
+						instance_exec(@item, &property[:cell_content])
 					end
 				end
 			end
