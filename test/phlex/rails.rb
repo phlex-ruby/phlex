e96ea1aaa6adff4688a96cca380e444ee056500e
Joel Drapper <joel@drapper.me>
2022-10-26 14:09:29 +0100

    Use normal Rubocop release (#292)
    * Use normal Rubocop release
    
    * Remove redundant string escapes

--- test/phlex/rails.rb
+++ test/phlex/rails.rb
@@ -8,7 +8,7 @@ describe Phlex::View do
 
 		it "renders the correct output" do
 			expect(output).to be ==
-				%(<p>Before</p>\n\n<h1>Hello World!</h1>\n\n<article class=\"drop-shadow p-5 rounded\">\n  <p>Start Card A</p>\n  <h3 class=\"font-bold\">Hello from A</h3>\n  <article class=\"drop-shadow p-5 rounded\">\n    <p>Start Card B</p>\n    <h3 class=\"font-bold\">Hello from B</h3>\n    <p>End Card B</p>\n</article>  <p>End Card A</p>\n</article>)
+				%(<p>Before</p>\n\n<h1>Hello World!</h1>\n\n<article class="drop-shadow p-5 rounded">\n  <p>Start Card A</p>\n  <h3 class="font-bold">Hello from A</h3>\n  <article class="drop-shadow p-5 rounded">\n    <p>Start Card B</p>\n    <h3 class="font-bold">Hello from B</h3>\n    <p>End Card B</p>\n</article>  <p>End Card A</p>\n</article>)
 		end
 	end
 end
