35e9b14351f31e64cf373e0ae9be25196950125b
stephann <3025661+stephannv@users.noreply.github.com>
2022-11-16 21:28:15 -0300

    Adapt test generator to new gem structure

--- phlex-rails/lib/generators/rspec/view/view_generator.rb
+++ phlex-rails/lib/generators/rspec/view/view_generator.rb
@@ -6,6 +6,7 @@ module Rspec
 			source_root File.expand_path("templates", __dir__)
 
 			def create_view
+				@path = File.join("app/views", class_path, "#{file_name}.rb")
 				template "view_spec.rb.erb", File.join("spec/views", class_path, "#{file_name}_spec.rb")
 			end
 		end
--- phlex-rails/lib/generators/test_unit/view_generator.rb
+++ phlex-rails/lib/generators/test_unit/view_generator.rb
@@ -5,6 +5,7 @@ module TestUnit
 		source_root File.expand_path("templates", __dir__)
 
 		def create_view
+			@path = File.join("app/views", class_path, "#{file_name}.rb")
 			template "view_test.rb.erb", File.join("test/views", class_path, "#{file_name}_test.rb")
 		end
 	end
