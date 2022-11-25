3e1aeaeb01638a59f99b0d0436e94a92735a6dbc
Brad Schrag <brad@cityinnovate.com>
2022-11-24 20:36:13 -0700

    Add then/else conditional token option
    * adds ability for tokens definitions to define the negative conditional result using a :then/:else hash
    * adds TokenGenerator class for handling the new logic
    * extends test coverage to test for negative token selection

--- test/phlex/view/tokens.rb
+++ test/phlex/view/tokens.rb
@@ -43,5 +43,51 @@ describe Phlex::HTML do
 					%(<a href="/" class="a b c true">Home</a>)
 			end
 		end
+
+		with "negative conditionals" do
+			view do
+				def template
+					a href: "/", **classes("a", "b", "c",
+						active?: {
+							then: "active",
+							else: "inactive"
+						}) do
+						"Home"
+					end
+				end
+
+				def active?
+					false
+				end
+			end
+
+			it "works" do
+				expect(output).to be ==
+					%(<a href="/" class="a b c inactive">Home</a>)
+			end
+		end
+
+		with "no truthy conditionals" do
+			view do
+				def template
+					a href: "/", **classes(
+						active?: {
+							then: "active"
+						}
+					) do
+						"Home"
+					end
+				end
+
+				def active?
+					false
+				end
+			end
+
+			it "works" do
+				expect(output).to be ==
+					%(<a href="/">Home</a>)
+			end
+		end
 	end
 end
