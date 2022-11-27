5bcb2aed8a0e8c5eea2c1450caa025a224a354b5
Joel Drapper <joel@drapper.me>
2022-11-27 17:08:01 +0000

    `format_object` method

---
+++ test/phlex/view/format_object.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require "date"
+
+class Example < Phlex::HTML
+	def template
+		h1 { Date.today }
+	end
+
+	private def format_object(object)
+		case object
+			when Date then object.strftime("%d/%m/%y")
+			else super
+		end
+	end
+end
+
+describe Phlex::HTML do
+	it "supports formatting objects" do
+		expect(Example.new.call).to be == "<h1>28/11/22</h1>"
+	end
+end
