35de19778574e892aa749c15be2c4241b4f591fb
Joel Drapper <joel@drapper.me>
2023-01-15 11:04:47 +0000

    Improve `DeferredRender`

--- lib/phlex/deferred_render.rb
+++ lib/phlex/deferred_render.rb
@@ -3,10 +3,5 @@
 module Phlex
 	module DeferredRender
 		include Experimental
-
-		def template(&block)
-			capture(&block) # yield the block and throw away the output
-			super() # empty parens ensure we don't pass the block which could be yielded a second time
-		end
 	end
 end
