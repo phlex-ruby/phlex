f8e9cbf674ecfe38977e0a73e49d4e5e01cf4da5
Will Cosgrove <will@cosgrove.email>
2023-01-20 12:29:05 -0600

    Fix using keyword arguments in initializer
    The override of `new` added in #438 did not support keyword arguments,
    and would cause an ArgumentError when called with them.

--- test/phlex/view/new.rb
+++ test/phlex/view/new.rb
@@ -6,6 +6,24 @@ class Example < Phlex::HTML
 	end
 end
 
+class ExampleWithArgs < Phlex::HTML
+	def initialize(name, should_render: false)
+		@name = name
+		@should_render = should_render
+	end
+
+	def render?
+		@should_render
+	end
+
+	def template
+		h1 {
+			yield
+			text(", #{@name}")
+		}
+	end
+end
+
 describe Phlex::HTML do
 	extend ViewHelper
 
@@ -18,6 +36,18 @@ describe Phlex::HTML do
 
 		it "treats the block as content" do
 			expect(output).to be == "<h1>Hello</h1>"
+		end
+	end
+
+	with "a block and arguments passed to new" do
+		view do
+			def template
+				render ExampleWithArgs.new("World", should_render: true) { "Hello" }
+			end
+		end
+
+		it "treats the block as content, and passes the args on" do
+			expect(output).to be == "<h1>Hello, World</h1>"
 		end
 	end
 end
