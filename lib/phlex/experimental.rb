19a327ce9d8409fda132aa80908658bda1d05377
Joel Drapper <joel@drapper.me>
2023-01-23 23:36:05 +0000

    Update experimental warning

--- lib/phlex/experimental.rb
+++ lib/phlex/experimental.rb
@@ -4,7 +4,7 @@ module Phlex
 	module Experimental
 		def before_template
 			if Phlex.configuration.experimental_warnings
-				puts "Warning: #{self.class.name} is experimental and subject to change."
+				puts "Warning: #{self.class.name} is using experimental Phlex features that are subject to change."
 			end
 
 			super
