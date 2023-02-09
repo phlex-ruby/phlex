cc69797350af3a5f1c5cf598a454b37c7ee1862c
Joel Drapper <joel@drapper.me>
2023-02-09 15:26:16 +0000

    Add YARD docs for HTML tags

--- lib/phlex/html.rb
+++ lib/phlex/html.rb
@@ -8,129 +8,18 @@ module Phlex
 	class HTML
 		DOCTYPE = "<!DOCTYPE html>"
 
-		STANDARD_ELEMENTS = {
-			a: "a",
-			abbr: "abbr",
-			address: "address",
-			article: "article",
-			aside: "aside",
-			b: "b",
-			bdi: "bdi",
-			bdo: "bdo",
-			blockquote: "blockquote",
-			body: "body",
-			button: "button",
-			caption: "caption",
-			cite: "cite",
-			code: "code",
-			colgroup: "colgroup",
-			data: "data",
-			datalist: "datalist",
-			dd: "dd",
-			del: "del",
-			details: "details",
-			dfn: "dfn",
-			dialog: "dialog",
-			div: "div",
-			dl: "dl",
-			dt: "dt",
-			em: "em",
-			fieldset: "fieldset",
-			figcaption: "figcaption",
-			figure: "figure",
-			footer: "footer",
-			form: "form",
-			g: "g",
-			h1: "h1",
-			h2: "h2",
-			h3: "h3",
-			h4: "h4",
-			h5: "h5",
-			h6: "h6",
-			head: "head",
-			header: "header",
-			hgroup: "hgroup",
-			html: "html",
-			i: "i",
-			iframe: "iframe",
-			ins: "ins",
-			kbd: "kbd",
-			label: "label",
-			legend: "legend",
-			li: "li",
-			main: "main",
-			map: "map",
-			mark: "mark",
-			menuitem: "menuitem",
-			meter: "meter",
-			nav: "nav",
-			noscript: "noscript",
-			object: "object",
-			ol: "ol",
-			optgroup: "optgroup",
-			option: "option",
-			output: "output",
-			p: "p",
-			path: "path",
-			picture: "picture",
-			pre: "pre",
-			progress: "progress",
-			q: "q",
-			rp: "rp",
-			rt: "rt",
-			ruby: "ruby",
-			s: "s",
-			samp: "samp",
-			script: "script",
-			section: "section",
-			select: "select",
-			slot: "slot",
-			small: "small",
-			span: "span",
-			strong: "strong",
-			style: "style",
-			sub: "sub",
-			summary: "summary",
-			sup: "sup",
-			svg: "svg",
-			table: "table",
-			tbody: "tbody",
-			td: "td",
-			template_tag: "template",
-			textarea: "textarea",
-			tfoot: "tfoot",
-			th: "th",
-			thead: "thead",
-			time: "time",
-			title: "title",
-			tr: "tr",
-			u: "u",
-			ul: "ul",
-			video: "video",
-			wbr: "wbr",
-		}.freeze
-
-		VOID_ELEMENTS = {
-			area: "area",
-			br: "br",
-			embed: "embed",
-			hr: "hr",
-			img: "img",
-			input: "input",
-			link: "link",
-			meta: "meta",
-			param: "param",
-			source: "source",
-			track: "track",
-			col: "col",
-		}.freeze
+		STANDARD_ELEMENTS = Concurrent::Map.new
+		VOID_ELEMENTS = Concurrent::Map.new
 
 		EVENT_ATTRIBUTES = %w[onabort onafterprint onbeforeprint onbeforeunload onblur oncanplay oncanplaythrough onchange onclick oncontextmenu oncopy oncuechange oncut ondblclick ondrag ondragend ondragenter ondragleave ondragover ondragstart ondrop ondurationchange onemptied onended onerror onfocus onhashchange oninput oninvalid onkeydown onkeypress onkeyup onload onloadeddata onloadedmetadata onloadstart onmessage onmousedown onmousemove onmouseout onmouseover onmouseup onmousewheel onoffline ononline onpagehide onpageshow onpaste onpause onplay onplaying onpopstate onprogress onratechange onreset onresize onscroll onsearch onseeked onseeking onselect onstalled onstorage onsubmit onsuspend ontimeupdate ontoggle onunload onvolumechange onwaiting onwheel].to_h { [_1, true] }.freeze
 
 		UNBUFFERED_MUTEX = Mutex.new
 
 		extend Elements
+
 		include Helpers
+		include VoidElements
+		include StandardElements
 
 		class << self
 			def call(...)
@@ -218,14 +107,6 @@ module Phlex
 
 		def format
 			:html
-		end
-
-		STANDARD_ELEMENTS.each do |method_name, tag|
-			register_element(method_name, tag: tag)
-		end
-
-		VOID_ELEMENTS.each do |method_name, tag|
-			register_void_element(method_name, tag: tag)
 		end
 
 		def text(content)
