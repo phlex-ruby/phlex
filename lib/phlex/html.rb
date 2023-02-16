1a763adb813c89234fe8f50a290f562834a2d6ca
Dave Kroondyk <davekaro@hey.com>
2023-02-15 22:33:42 -0500

    Move refinement to superclass

--- lib/phlex/html.rb
+++ lib/phlex/html.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-if Gem::Version.new(RUBY_VERSION) < Gem::Version.new("3.0")
-	using Phlex::Overrides::Symbol::Name
-end
-
 module Phlex
 	class HTML < XML
 		# A list of HTML attributes that have the potential to execute unsafe JavaScript.
--- lib/phlex/xml.rb
+++ lib/phlex/xml.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+if Gem::Version.new(RUBY_VERSION) < Gem::Version.new("3.0")
+	using Phlex::Overrides::Symbol::Name
+end
+
 module Phlex
 	class XML
 		class << self
