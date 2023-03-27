ce3ff057076175336f5aa43c8c15b6aff990512d
Will Cosgrove <will@cosgrove.email>
2023-03-27 15:00:42 -0500

    Remove `text` in favor of `plain`

--- test/phlex/view/plain.rb
+++ test/phlex/view/plain.rb
@@ -3,19 +3,6 @@
 describe Phlex::HTML do
 	extend ViewHelper
 
-	with "deprecated text method" do
-		view do
-			def template
-				text "Depreacted"
-			end
-		end
-
-		it "renders content and prints deprecation warning" do
-			expect(example).to receive(:warn).with("DEPRECATED: The `text` method has been deprecated in favour of `plain`. Please use `plain` instead. The `text` method will be removed in a future version of Phlex. Called from: test/phlex/view/text.rb:9:in `template'")
-			expect(output).to be == "Depreacted"
-		end
-	end
-
 	with "text" do
 		view do
 			def template
