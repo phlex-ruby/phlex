679557180bff6defb41c449a67c7e75b5f8a1f6f
Joel Drapper <joel@drapper.me>
2023-05-03 12:00:34 +0100

    Convert attributes test to GreenDots

--- gd/support/helper.rb
+++ gd/support/helper.rb
@@ -1,3 +1,23 @@
 # frozen_string_literal: true
 
 require "phlex"
+require "bundler"
+
+Bundler.require :test
+
+module TestHelper
+	def build_component_with_template(&block)
+		Class.new(Phlex::HTML) { define_method(:template, &block) }
+	end
+end
+
+module ToRender
+	def to_render(expected_output)
+		output = subject.call
+		assert(output == expected_output) { "Expected `#{output.inspect}` to equal `#{expected_output.inspect}`." }
+	end
+end
+
+GreenDots.configure do |config|
+	config.register_matcher ToRender, Phlex::SGML
+end
