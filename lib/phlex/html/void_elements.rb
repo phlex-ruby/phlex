29fb05bcdab953b278fad8a2c607416208272a9f
Will Cosgrove <will@cosgrove.email>
2024-01-31 04:26:36 -0600

    Simplify Element Registration (cont.) (#642)

--- lib/phlex/html/void_elements.rb
+++ lib/phlex/html/void_elements.rb
@@ -8,71 +8,71 @@ module Phlex::HTML::VoidElements
 	# 	Outputs an `<area>` tag.
 	# 	@return [nil]
 	# 	@see https://developer.mozilla.org/docs/Web/HTML/Element/area
-	register_void_element :area, tag: "area"
+	register_void_element :area
 
 	# @!method br(**attributes, &content)
 	# 	Outputs a `<br>` tag.
 	# 	@return [nil]
 	# 	@see https://developer.mozilla.org/docs/Web/HTML/Element/br
-	register_void_element :br, tag: "br"
+	register_void_element :br
 
 	# @!method col(**attributes, &content)
 	# 	Outputs a `<col>` tag.
 	# 	@return [nil]
 	# 	@see https://developer.mozilla.org/docs/Web/HTML/Element/col
-	register_void_element :col, tag: "col"
+	register_void_element :col
 
 	# @!method embed(**attributes, &content)
 	# 	Outputs an `<embed>` tag.
 	# 	@return [nil]
 	# 	@see https://developer.mozilla.org/docs/Web/HTML/Element/embed
-	register_void_element :embed, tag: "embed"
+	register_void_element :embed
 
 	# @!method hr(**attributes, &content)
 	# 	Outputs an `<hr>` tag.
 	# 	@return [nil]
 	# 	@see https://developer.mozilla.org/docs/Web/HTML/Element/hr
-	register_void_element :hr, tag: "hr"
+	register_void_element :hr
 
 	# @!method img(**attributes, &content)
 	# 	Outputs an `<img>` tag.
 	# 	@return [nil]
 	# 	@see https://developer.mozilla.org/docs/Web/HTML/Element/img
-	register_void_element :img, tag: "img"
+	register_void_element :img
 
 	# @!method input(**attributes, &content)
 	# 	Outputs an `<input>` tag.
 	# 	@return [nil]
 	# 	@see https://developer.mozilla.org/docs/Web/HTML/Element/input
-	register_void_element :input, tag: "input"
+	register_void_element :input
 
 	# @!method link(**attributes, &content)
 	# 	Outputs a `<link>` tag.
 	# 	@return [nil]
 	# 	@see https://developer.mozilla.org/docs/Web/HTML/Element/link
-	register_void_element :link, tag: "link"
+	register_void_element :link
 
 	# @!method meta(**attributes, &content)
 	# 	Outputs a `<meta>` tag.
 	# 	@return [nil]
 	# 	@see https://developer.mozilla.org/docs/Web/HTML/Element/meta
-	register_void_element :meta, tag: "meta"
+	register_void_element :meta
 
 	# @!method param(**attributes, &content)
 	# 	Outputs a `<param>` tag.
 	# 	@return [nil]
 	# 	@see https://developer.mozilla.org/docs/Web/HTML/Element/param
-	register_void_element :param, tag: "param", deprecated: true
+	register_void_element :param, deprecated: true
 
 	# @!method source(**attributes, &content)
 	# 	Outputs a `<source>` tag.
 	# 	@return [nil]
 	# 	@see https://developer.mozilla.org/docs/Web/HTML/Element/source
-	register_void_element :source, tag: "source"
+	register_void_element :source
 
 	# @!method track(**attributes, &content)
 	# 	Outputs a `<track>` tag.
 	# 	@return [nil]
 	# 	@see https://developer.mozilla.org/docs/Web/HTML/Element/track
-	register_void_element :track, tag: "track"
+	register_void_element :track
 end
