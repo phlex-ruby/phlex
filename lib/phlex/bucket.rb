1d0a65d2f1e2db1c0060729ff0e89aff9ed8e880
Joel Drapper <joel@drapper.me>
2024-03-14 12:27:06 +0000

    Lazy load autoloaded bucket components

--- lib/phlex/bucket.rb
+++ lib/phlex/bucket.rb
@@ -5,37 +5,48 @@ module Phlex::Bucket
 		warn "🚨 [WARNING] Phlex::Bucket is experimental and may be removed from future versions of Phlex."
 
 		# Eager load all constants in the module for apps that use Zeitwerk.
-		mod.constants.each { |c| mod.const_get(c) }
+		mod.constants.each { |c| mod.const_added(c) if mod.autoload?(c) }
 	end
 
 	def const_added(name)
-		# This can sometime be triggered by an autoload, which means it gets
-		# triggered a second time when we call `const_get` below and Ruby loads it.
-		return super if Fiber[:__phlex_adding_bucket_const__]
-
-		begin
-			Fiber[:__phlex_adding_bucket_const__] = true
-			constant = const_get(name)
-		ensure
-			Fiber[:__phlex_adding_bucket_const__] = false
-		end
-
-		if instance_methods.include?(name)
-			raise NameError, "The instance method `#{name}' is already defined on `#{inspect}`."
-		elsif methods.include?(name)
-			raise NameError, "The method `#{name}' is already defined on `#{inspect}`."
-		end
-
-		constant.include(self)
-
-		if Class === constant && constant < Phlex::SGML
+		if autoload?(name)
 			define_method(name) do |*args, **kwargs, &block|
-				render(constant.new(*args, **kwargs), &block)
+				this = method(name)
+				self.class.const_get(name) # load the constant (which should override this method)
+
+				if this == method(name)
+					# loading the constant didn't override this method
+					super
+				else
+					public_send(name, *args, **kwargs, &block) # call this method again
+				end
 			end
 
 			define_singleton_method(name) do |*args, **kwargs, &block|
-				Fiber[:__phlex_component__].instance_exec do
+				this = method(name)
+				const_get(name) # load the constant (which should override this method)
+
+				if this == method(name)
+					# loading the constant didn't override this method
+					super
+				else
+					public_send(name, *args, **kwargs, &block) # call this method again
+				end
+			end
+		else
+			constant = const_get(name)
+
+			constant.include(self)
+
+			if Class === constant && constant < Phlex::SGML
+				define_method(name) do |*args, **kwargs, &block|
 					render(constant.new(*args, **kwargs), &block)
+				end
+
+				define_singleton_method(name) do |*args, **kwargs, &block|
+					Fiber[:__phlex_component__].instance_exec do
+						render(constant.new(*args, **kwargs), &block)
+					end
 				end
 			end
 		end
