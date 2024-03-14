279f1ce29efcc652dc586cf33eb404023190a828
Joel Drapper <joel@drapper.me>
2024-03-14 13:01:02 +0000

    Use method_missing

--- lib/phlex/bucket.rb
+++ lib/phlex/bucket.rb
@@ -1,52 +1,60 @@
 # frozen_string_literal: true
 
 module Phlex::Bucket
+	module Proxy
+		def method_missing(name, *args, **kwargs, &block)
+			return super unless self.class.constants.include?(name)
+			constant = self.class.const_get(name)
+
+			if methods.include?(name)
+				public_send(name, *args, **kwargs, &block)
+			else
+				super
+			end
+		end
+	end
+
 	def self.extended(mod)
 		warn "🚨 [WARNING] Phlex::Bucket is experimental and may be removed from future versions of Phlex."
+		mod.include(Proxy)
+	end
 
-		# Eager load all constants in the module for apps that use Zeitwerk.
-		mod.constants.each { |c| mod.const_added(c) if mod.autoload?(c) }
+	def method_missing(name, *args, **kwargs, &block)
+		return super unless constants.include?(name)
+		constant = const_get(name)
+
+		if methods.include?(name)
+			public_send(name, *args, **kwargs, &block)
+		else
+			super
+		end
+	end
+
+	def respond_to_missing?(name, include_private = false)
+		autoload?(name) || super
 	end
 
 	def const_added(name)
-		if autoload?(name)
-			define_method(name) do |*args, **kwargs, &block|
-				this = method(name)
-				self.class.const_get(name) # load the constant (which should override this method)
+		return if autoload?(name)
 
-				if this == method(name)
-					# loading the constant didn't override this method
-					super
-				else
-					public_send(name, *args, **kwargs, &block) # call this method again
-				end
+		constant = const_get(name)
+
+		if instance_methods.include?(name)
+			raise NameError, "The instance method `#{name}' is already defined on `#{inspect}`."
+		elsif methods.include?(name)
+			raise NameError, "The method `#{name}' is already defined on `#{inspect}`."
+		end
+
+		constant.include(self)
+
+		if Class === constant && constant < Phlex::SGML
+			define_method(name) do |*args, **kwargs, &block|
+				render(constant.new(*args, **kwargs), &block)
 			end
 
 			define_singleton_method(name) do |*args, **kwargs, &block|
-				this = method(name)
-				const_get(name) # load the constant (which should override this method)
-
-				if this == method(name)
-					# loading the constant didn't override this method
-					super
-				else
-					public_send(name, *args, **kwargs, &block) # call this method again
-				end
-			end
-		else
-			constant = const_get(name)
-
-			constant.include(self)
-
-			if Class === constant && constant < Phlex::SGML
-				define_method(name) do |*args, **kwargs, &block|
+				Fiber[:__phlex_component__].instance_exec do
 					render(constant.new(*args, **kwargs), &block)
-				end
-
-				define_singleton_method(name) do |*args, **kwargs, &block|
-					Fiber[:__phlex_component__].instance_exec do
-						render(constant.new(*args, **kwargs), &block)
-					end
 				end
 			end
 		end
