93b3361ce7e542614facd95c34f2e0431aa6b101
Joel Drapper <joel@drapper.me>
2024-03-27 21:24:03 +0000

    Update await.rb

--- test/phlex/sgml/await.rb
+++ test/phlex/sgml/await.rb
@@ -17,23 +17,26 @@ class Example < Phlex::HTML
 end
 
 describe Phlex::SGML do
-	describe "with Async tasks" do
-		it "flushes when waiting" do
-			Sync do
-				task = Async { sleep 0; "Hello" }
-				buffer = []
-				Example.new(task).call(buffer)
-				expect(buffer).to be == (["<h1>Before</h1>", "<h1>Hello</h1><h1>After</h1>"])
+	# The Async gem only works with CRuby 3.0 and above.
+	if RUBY_ENGINE == "ruby" && RUBY_VERSION[0] > "3"
+		describe "with Async tasks" do
+			it "flushes when waiting" do
+				Sync do
+					task = Async { sleep 0; "Hello" }
+					buffer = []
+					Example.new(task).call(buffer)
+					expect(buffer).to be == (["<h1>Before</h1>", "<h1>Hello</h1><h1>After</h1>"])
+				end
 			end
-		end
 
-		it "doesn't flush when it doesn't need to wait" do
-			Sync do
-				task = Async { sleep 0; "Hello" }
-				task.wait
-				buffer = []
-				Example.new(task).call(buffer)
-				expect(buffer).to be == (["<h1>Before</h1><h1>Hello</h1><h1>After</h1>"])
+			it "doesn't flush when it doesn't need to wait" do
+				Sync do
+					task = Async { sleep 0; "Hello" }
+					task.wait
+					buffer = []
+					Example.new(task).call(buffer)
+					expect(buffer).to be == (["<h1>Before</h1><h1>Hello</h1><h1>After</h1>"])
+				end
 			end
 		end
 	end
