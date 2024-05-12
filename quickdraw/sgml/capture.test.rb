aa7cc52bb603cd5837b90f31425e96c2dea16f3d
Joel Drapper <joel@drapper.me>
2024-05-12 15:51:30 +0100

    Test capture with arguments

---
+++ quickdraw/sgml/capture.test.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class ExampleCaptureWithArguments < Phlex::HTML
+	def view_template(&block)
+		h1 { capture("a", "b", "c", &block) }
+	end
+end
+
+test "arguments are passed to the capture block" do
+	output = ExampleCaptureWithArguments.new.call do |a, b, c|
+		"#{a} #{b} #{c}"
+	end
+
+	expect(output) == "<h1>a b c</h1>"
+end
