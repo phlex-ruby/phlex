a0103084b9fb3409c5917f3f18bd489a1f75a882
Joel Drapper <joel@drapper.me>
2024-06-10 13:02:59 +0100

    Improved lazy loading for kits

--- lib/phlex/kit.rb
+++ lib/phlex/kit.rb
@@ -1,42 +1,39 @@
 # frozen_string_literal: true
 
 module Phlex::Kit
-	def self.extended(mod)
-		warn "⚠️ [WARNING] Phlex::Kit is experimental and may be removed from future versions of Phlex."
-
-		mod.define_method(:respond_to_missing?) do |name, _include_private = false|
-			if name[0] == name[0].upcase && mod.constants.include?(name) && mod.const_get(name) && methods.include?(name)
-				true
-			else
-				super
-			end
-		end
-
-		mod.define_method(:method_missing) do |name, *args, **kwargs, &block|
-			if name[0] == name[0].upcase && mod.constants.include?(name) && mod.const_get(name) && methods.include?(name)
+	module LazyLoader
+		def method_missing(name, *args, **kwargs, &block)
+			if name[0] == name[0].upcase && __phlex_kit_constants__.include?(name) && __get_phlex_kit_constant__(name) && methods.include?(name)
 				public_send(name, *args, **kwargs, &block)
 			else
 				super
 			end
 		end
 
-		super
-	end
-
-	def method_missing(name, *args, **kwargs, &block)
-		if name[0] == name[0].upcase && constants.include?(name) && const_get(name) && methods.include?(name)
-			public_send(name, *args, **kwargs, &block)
-		else
-			super
+		def respond_to_missing?(name, include_private = false)
+			if name[0] == name[0].upcase && __phlex_kit_constants__.include?(name) && __get_phlex_kit_constant__(name) && methods.include?(name)
+				true
+			else
+				super
+			end
 		end
 	end
 
-	def respond_to_missing?(name, include_private = false)
-		if name[0] == name[0].upcase && constants.include?(name) && const_get(name) && methods.include?(name)
-			true
-		else
-			super
-		end
+	include LazyLoader
+
+	def self.extended(mod)
+		warn "⚠️ [WARNING] Phlex::Kit is experimental and may be removed from future versions of Phlex."
+		mod.include(LazyLoader)
+		mod.define_method(:__phlex_kit_constants__) { mod.__phlex_kit_constants__ }
+		mod.define_method(:__get_phlex_kit_constant__) { |name| mod.__get_phlex_kit_constant__(name) }
+	end
+
+	def __phlex_kit_constants__
+		constants
+	end
+
+	def __get_phlex_kit_constant__(name)
+		const_get(name)
 	end
 
 	def const_added(name)
