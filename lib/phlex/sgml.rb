bd0f82eeb7cfa85c63190122ac28cda905b3f52d
Joel Drapper <joel@drapper.me>
2024-09-02 20:15:58 +0100

    Update lib/phlex/sgml.rb
    Co-authored-by: Will Cosgrove <will@cosgrove.email>
    Signed-off-by: Joel Drapper <joel@drapper.me>

--- lib/phlex/sgml.rb
+++ lib/phlex/sgml.rb
@@ -228,7 +228,7 @@ module Phlex
 			normalized_name = case name
 				when Symbol then name.name.downcase
 				when String then name.downcase
-				else raise ArgumentError.new("Expected the tag namea as a Symbol or String.")
+				else raise ArgumentError.new("Expected the tag name as a Symbol or String.")
 			end
 
 			if registered_elements[normalized_name]
