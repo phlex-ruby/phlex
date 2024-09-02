db587b7df0c4163e6e84446920ea56860e003923
Joel Drapper <joel@drapper.me>
2024-09-02 20:19:04 +0100

    More test stuff

--- quickdraw/html.test.rb
+++ quickdraw/html.test.rb
@@ -1,10 +1,5 @@
 # frozen_string_literal: true
 
-test "default view_template" do
-	component = Class.new(Phlex::HTML)
-	expect(component.new.call) == ""
-end
-
 test "content type" do
 	component = Class.new(Phlex::HTML)
 	expect(component.new.content_type) == "text/html"
