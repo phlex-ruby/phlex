815c94ea7d04abca03d925b730bd0ae41fc234b4
Will Cosgrove <will@cosgrove.email>
2024-09-06 18:38:02 -0500

    Allow elements to output safe return values (#773)
    ### Before
    
    ```ruby
    script { safe(%(console.log("Hi 👋"))) } # => <script></script>
    ```
    
    ### After
    ```ruby
    script { safe(%(console.log("Hi 👋"))) } # => <script>console.log("Hi 👋")</script>
    ```

--- quickdraw/sgml/safe_value.test.rb
+++ quickdraw/sgml/safe_value.test.rb
@@ -12,3 +12,13 @@ end
 test "safe value" do
 	expect(Example.call) == %(<a onclick="window.history.back()" href="javascript:window.history.back()"></a>)
 end
+
+class ExampleScript < Phlex::HTML
+  def view_template
+    script { safe(%(console.log("Hello World");)) }
+  end
+end
+
+test "element content blocks that return safe values" do
+  expect(ExampleScript.call) == %(<script>console.log("Hello World");</script>)
+end
