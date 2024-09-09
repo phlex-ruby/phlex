bfadd176a5c5b33d112f360dd54956a97c900e75
Joel Drapper <joel@drapper.me>
2024-09-09 03:15:46 +0100

    Update safe_value.test.rb

--- quickdraw/sgml/safe_value.test.rb
+++ quickdraw/sgml/safe_value.test.rb
@@ -14,11 +14,11 @@ test "safe value" do
 end
 
 class ExampleScript < Phlex::HTML
-  def view_template
-    script { safe(%(console.log("Hello World");)) }
-  end
+	def view_template
+		script { safe(%(console.log("Hello World");)) }
+	end
 end
 
 test "element content blocks that return safe values" do
-  expect(ExampleScript.call) == %(<script>console.log("Hello World");</script>)
+	expect(ExampleScript.call) == %(<script>console.log("Hello World");</script>)
 end
