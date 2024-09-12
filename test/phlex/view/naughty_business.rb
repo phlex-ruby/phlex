b0694c7d64cefb6071690778a1f6910ad8c4e623
Joel Drapper <joel@drapper.me>
2024-09-12 11:50:53 +0200

    `srcdoc` should be considered unsafe in version of Phlex that don’t escape attribute values (#777)
    Since we replaced the HTML escape with a `"` gsub, we should consider
    `srcdoc` unsafe.

--- test/phlex/view/naughty_business.rb
+++ test/phlex/view/naughty_business.rb
@@ -157,7 +157,7 @@ describe Phlex::HTML do
 		end
 	end
 
-	Phlex::HTML::EVENT_ATTRIBUTES.each do |event_attribute|
+	Phlex::HTML::UNSAFE_ATTRIBUTES.each do |event_attribute|
 		with "with naughty #{event_attribute} attribute" do
 			naughty_attributes = { event_attribute => "alert(1);" }
 
