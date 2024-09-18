6c8b60231852d435cd398fd17bf8ffb859cb61ed
Joel Drapper <joel@drapper.me>
2024-09-18 15:48:30 +0100

    Update svg.test.rb

--- quickdraw/html/svg.test.rb
+++ quickdraw/html/svg.test.rb
@@ -8,6 +8,16 @@ class Example < Phlex::HTML
 	end
 end
 
+class ExampleWithoutContent < Phlex::HTML
+	def view_template
+		svg
+	end
+end
+
+test "rendering SVG without content" do
+	expect(ExampleWithoutContent.call) == %(<svg></svg>)
+end
+
 test "rendering SVG inside HTML components" do
 	expect(Example.call) == %(<svg><path d="321"></path></svg>)
 end
