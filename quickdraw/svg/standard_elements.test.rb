646757e6a17698c0dea341d40a425cbbfd214222
Joel Drapper <joel@drapper.me>
2024-09-18 22:35:54 +0100

    Create SVG standard elements test

---
+++ quickdraw/svg/standard_elements.test.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+Phlex::SVG::StandardElements.__registered_elements__.each do |method_name, tag|
+	test "<#{tag}> called with an underscore prefix while overridden" do
+		example = Class.new(Phlex::SVG) do
+			define_method :view_template do
+				__send__("_#{method_name}")
+			end
+
+			define_method method_name do
+				super(class: "overridden")
+			end
+		end
+
+		expect(example.call) == %(<#{tag}></#{tag}>)
+	end
+
+	test "<#{tag}> with block text content and attributes" do
+		example = Class.new(Phlex::SVG) do
+			define_method :view_template do
+				__send__(method_name, class: "class", id: "id", disabled: true, selected: false) { "content" }
+			end
+		end
+
+		expect(example.call) == %(<#{tag} class="class" id="id" disabled>content</#{tag}>)
+	end
+
+	test "<#{tag}> with string attribute keys" do
+		example = Class.new(Phlex::SVG) do
+			define_method :view_template do
+				__send__(method_name, "attribute_with_underscore" => true) { "content" }
+			end
+		end
+
+		expect(example.call) == %(<#{tag} attribute_with_underscore>content</#{tag}>)
+	end
+
+	test "<#{tag}> with hash attribute values" do
+		example = Class.new(Phlex::SVG) do
+			define_method :view_template do
+				__send__(method_name, aria: { hidden: true }, data_turbo: { frame: "_top" }) { "content" }
+			end
+		end
+
+		expect(example.call) == %(<#{tag} aria-hidden data-turbo-frame="_top">content</#{tag}>)
+	end
+end
