9763888a62e6728b409242e32e76b910a8c721bb
Joel Drapper <joel@drapper.me>
2024-10-03 11:57:01 +0100

    Begin adding LSP hints to SVG elements

--- lib/phlex/svg/standard_elements.rb
+++ lib/phlex/svg/standard_elements.rb
@@ -5,321 +5,480 @@ module Phlex::SVG::StandardElements
 
 	# Outputs an `<a>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/a
-	def a(**attributes, &content) = nil
-	register_element :a
+	register_element def a(
+		download: nil,
+		href: nil,
+		hreflang: nil,
+		ping: nil,
+		referrerpolicy: nil,
+		rel: nil,
+		target: nil,
+		type: nil,
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs an `<animate>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/animate
-	def animate(**attributes, &content) = nil
-	register_element :animate
+	register_element def animate(
+		attributeName: nil,
+		values: nil,
+		dur: nil,
+		repeatCount: nil,
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs an `<animateMotion>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/animateMotion
-	def animateMotion(**attributes, &content) = nil
-	register_element :animateMotion
+	register_element def animateMotion(
+		keyPoints: nil,
+		path: nil,
+		rotate: nil,
+		begin: nil,
+		dur: nil,
+		end: nil,
+		repeatCount: nil,
+		repeatDur: nil,
+		fill: nil,
+		calcMode: nil,
+		values: nil,
+		keyTimes: nil,
+		keySplines: nil,
+		from: nil,
+		to: nil,
+		by: nil,
+		attributeName: nil,
+		additive: nil,
+		accumulate: nil,
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs an `<animateTransform>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/animateTransform
-	def animateTransform(**attributes, &content) = nil
-	register_element :animateTransform
+	register_element def animateTransform(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs a `<circle>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/circle
-	def circle(**attributes, &content) = nil
-	register_element :circle
+	register_element def circle(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs a `<clipPath>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/clipPath
-	def clipPath(**attributes, &content) = nil
-	register_element :clipPath
+	register_element def clipPath(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs a `<defs>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/defs
-	def defs(**attributes, &content) = nil
-	register_element :defs
+	register_element def defs(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs a `<desc>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/desc
-	def desc(**attributes, &content) = nil
-	register_element :desc
+	register_element def desc(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs a `<discard>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/discard
-	def discard(**attributes, &content) = nil
-	register_element :discard
+	register_element def discard(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs an `<ellipse>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/ellipse
-	def ellipse(**attributes, &content) = nil
-	register_element :ellipse
+	register_element def ellipse(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs an `<feBlend>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/feBlend
-	def feBlend(**attributes, &content) = nil
-	register_element :feBlend
+	register_element def feBlend(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs an `<feColorMatrix>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/feColorMatrix
-	def feColorMatrix(**attributes, &content) = nil
-	register_element :feColorMatrix
+	register_element def feColorMatrix(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs an `<feComponentTransfer>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/feComponentTransfer
-	def feComponentTransfer(**attributes, &content) = nil
-	register_element :feComponentTransfer
+	register_element def feComponentTransfer(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs an `<feComposite>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/feComposite
-	def feComposite(**attributes, &content) = nil
-	register_element :feComposite
+	register_element def feComposite(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs an `<feConvolveMatrix>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/feConvolveMatrix
-	def feConvolveMatrix(**attributes, &content) = nil
-	register_element :feConvolveMatrix
+	register_element def feConvolveMatrix(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs an `<feDiffuseLighting>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/feDiffuseLighting
-	def feDiffuseLighting(**attributes, &content) = nil
-	register_element :feDiffuseLighting
+	register_element def feDiffuseLighting(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs an `<feDisplacementMap>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/feDisplacementMap
-	def feDisplacementMap(**attributes, &content) = nil
-	register_element :feDisplacementMap
+	register_element def feDisplacementMap(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs an `<feDistantLight>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/feDistantLight
-	def feDistantLight(**attributes, &content) = nil
-	register_element :feDistantLight
+	register_element def feDistantLight(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs an `<feDropShadow>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/feDropShadow
-	def feDropShadow(**attributes, &content) = nil
-	register_element :feDropShadow
+	register_element def feDropShadow(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs an `<feFlood>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/feFlood
-	def feFlood(**attributes, &content) = nil
-	register_element :feFlood
+	register_element def feFlood(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs an `<feFuncA>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/feFuncA
-	def feFuncA(**attributes, &content) = nil
-	register_element :feFuncA
+	register_element def feFuncA(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs an `<feFuncB>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/feFuncB
-	def feFuncB(**attributes, &content) = nil
-	register_element :feFuncB
+	register_element def feFuncB(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs an `<feFuncG>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/feFuncG
-	def feFuncG(**attributes, &content) = nil
-	register_element :feFuncG
+	register_element def feFuncG(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs an `<feFuncR>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/feFuncR
-	def feFuncR(**attributes, &content) = nil
-	register_element :feFuncR
+	register_element def feFuncR(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs an `<feGaussianBlur>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/feGaussianBlur
-	def feGaussianBlur(**attributes, &content) = nil
-	register_element :feGaussianBlur
+	register_element def feGaussianBlur(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs an `<feImage>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/feImage
-	def feImage(**attributes, &content) = nil
-	register_element :feImage
+	register_element def feImage(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs an `<feMerge>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/feMerge
-	def feMerge(**attributes, &content) = nil
-	register_element :feMerge
+	register_element def feMerge(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs an `<feMergeNode>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/feMergeNode
-	def feMergeNode(**attributes, &content) = nil
-	register_element :feMergeNode
+	register_element def feMergeNode(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs an `<feMorphology>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/feMorphology
-	def feMorphology(**attributes, &content) = nil
-	register_element :feMorphology
+	register_element def feMorphology(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs an `<feOffset>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/feOffset
-	def feOffset(**attributes, &content) = nil
-	register_element :feOffset
+	register_element def feOffset(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs an `<fePointLight>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/fePointLight
-	def fePointLight(**attributes, &content) = nil
-	register_element :fePointLight
+	register_element def fePointLight(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs an `<feSpecularLighting>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/feSpecularLighting
-	def feSpecularLighting(**attributes, &content) = nil
-	register_element :feSpecularLighting
+	register_element def feSpecularLighting(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs an `<feSpotLight>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/feSpotLight
-	def feSpotLight(**attributes, &content) = nil
-	register_element :feSpotLight
+	register_element def feSpotLight(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs an `<feTile>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/feTile
-	def feTile(**attributes, &content) = nil
-	register_element :feTile
+	register_element def feTile(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs an `<feTurbulence>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/feTurbulence
-	def feTurbulence(**attributes, &content) = nil
-	register_element :feTurbulence
+	register_element def feTurbulence(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs a `<filter>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/filter
-	def filter(**attributes, &content) = nil
-	register_element :filter
+	register_element def filter(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs a `<foreignObject>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/foreignObject
-	def foreignObject(**attributes, &content) = nil
-	register_element :foreignObject
+	register_element def foreignObject(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs a `<g>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/g
-	def g(**attributes, &content) = nil
-	register_element :g
+	register_element def g(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs an `<image>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/image
-	def image(**attributes, &content) = nil
-	register_element :image
+	register_element def image(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs a `<line>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/line
-	def line(**attributes, &content) = nil
-	register_element :line
+	register_element def line(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs a `<linearGradient>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/linearGradient
-	def linearGradient(**attributes, &content) = nil
-	register_element :linearGradient
+	register_element def linearGradient(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs a `<marker>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/marker
-	def marker(**attributes, &content) = nil
-	register_element :marker
+	register_element def marker(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs a `<mask>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/mask
-	def mask(**attributes, &content) = nil
-	register_element :mask
+	register_element def mask(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs a `<metadata>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/metadata
-	def metadata(**attributes, &content) = nil
-	register_element :metadata
+	register_element def metadata(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs an `<mpath>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/mpath
-	def mpath(**attributes, &content) = nil
-	register_element :mpath
+	register_element def mpath(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs a `<path>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/path
-	def path(**attributes, &content) = nil
-	register_element :path
+	register_element def path(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs a `<pattern>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/pattern
-	def pattern(**attributes, &content) = nil
-	register_element :pattern
+	register_element def pattern(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs a `<polygon>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/polygon
-	def polygon(**attributes, &content) = nil
-	register_element :polygon
+	register_element def polygon(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs a `<polyline>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/polyline
-	def polyline(**attributes, &content) = nil
-	register_element :polyline
+	register_element def polyline(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs a `<radialGradient>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/radialGradient
-	def radialGradient(**attributes, &content) = nil
-	register_element :radialGradient
+	register_element def radialGradient(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs a `<rect>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/rect
-	def rect(**attributes, &content) = nil
-	register_element :rect
+	register_element def rect(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs a `<script>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/script
-	def script(**attributes, &content) = nil
-	register_element :script
+	register_element def script(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs a `<set>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/set
-	def set(**attributes, &content) = nil
-	register_element :set
+	register_element def set(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs a `<stop>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/stop
-	def stop(**attributes, &content) = nil
-	register_element :stop
+	register_element def stop(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs a `<style>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/style
-	def style(**attributes, &content) = nil
-	register_element :style
+	register_element def style(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs an `<svg>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/svg
-	def svg(**attributes, &content) = nil
-	register_element :svg
+	register_element def svg(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs a `<switch>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/switch
-	def switch(**attributes, &content) = nil
-	register_element :switch
+	register_element def switch(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs a `<symbol>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/symbol
-	def symbol(**attributes, &content) = nil
-	register_element :symbol
+	register_element def symbol(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs a `<text>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/text
-	def text(**attributes, &content) = nil
-	register_element :text
+	register_element def text(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs a `<textPath>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/textPath
-	def textPath(**attributes, &content) = nil
-	register_element :textPath
+	register_element def textPath(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs a `<title>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/title
-	def title(**attributes, &content) = nil
-	register_element :title
+	register_element def title(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs a `<tspan>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/tspan
-	def tspan(**attributes, &content) = nil
-	register_element :tspan
+	register_element def tspan(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs a `<use>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/use
-	def use(**attributes, &content) = nil
-	register_element :use
+	register_element def use(
+		**attributes,
+		&content
+	) = nil
 
 	# Outputs a `<view>` tag.
 	# See https://developer.mozilla.org/docs/Web/SVG/Element/view
-	def view(**attributes, &content) = nil
-	register_element :view
+	register_element def view(
+		**attributes,
+		&content
+	) = nil
 end
