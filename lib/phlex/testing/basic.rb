70873c7c10cff1b66dd4fb16218fa854acf6db5d
Seth Horsley <git-public.ch12j@seth4242.com>
2024-10-06 16:19:39 -0700

    Add basic testing
    Signed-off-by: Seth Horsley <git-public.ch12j@seth4242.com>

---
+++ lib/phlex/testing/basic.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module Phlex
+  module Testing
+    module Basic
+      def render(view, &)
+        if view.is_a?(Class) && view < Phlex::SGML
+          view = view.new
+        end
+
+        view.call(view_context:, &)
+      end
+
+      def view_context
+        nil
+      end
+    end
+
+    # Create an alias for the Basic module
+    ViewHelper = Basic
+  end
+end
