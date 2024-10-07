c63c6f2373b6285e2b206ddb3803436fa4dc5378
Seth Horsley <github.dvxhi@sl.isethi.com>
2024-10-07 02:27:46 +0200

    fix rubocop

--- lib/phlex/testing/basic.rb
+++ lib/phlex/testing/basic.rb
@@ -1,22 +1,22 @@
 # frozen_string_literal: true
 
 module Phlex
-  module Testing
-    module Basic
-      def render(view, &)
-        if view.is_a?(Class) && view < Phlex::SGML
-          view = view.new
-        end
+	module Testing
+		module Basic
+			def render(view, &)
+				if view.is_a?(Class) && view < Phlex::SGML
+					view = view.new
+				end
 
-        view.call(view_context:, &)
-      end
+				view.call(view_context:, &)
+			end
 
-      def view_context
-        nil
-      end
-    end
+			def view_context
+				nil
+			end
+		end
 
-    # Create an alias for the Basic module
-    ViewHelper = Basic
-  end
+		# Create an alias for the Basic module
+		ViewHelper = Basic
+	end
 end
