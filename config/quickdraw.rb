512f178c753f275930793fd47bcbe3316f45b5bc
Joel Drapper <joel@drapper.me>
2025-02-04 21:20:09 +0000

    Simplify the eager load

--- config/quickdraw.rb
+++ config/quickdraw.rb
@@ -19,26 +19,9 @@ end
 
 Bundler.require :test
 
-module AutoloadRecorder
-	AUTOLOADS = []
-
-	def self.load_all
-		AUTOLOADS.each do |(mod, name)|
-			mod.const_get(name)
-		end
-	end
-
-	def autoload(name, path)
-		super
-		::AutoloadRecorder::AUTOLOADS << [self, name].freeze
-	end
-end
-
-Module.prepend(AutoloadRecorder)
-
 require "phlex"
 
-AutoloadRecorder.load_all
+Phlex.eager_load
 
 # Previous content of test helper now starts here
 $LOAD_PATH.unshift(File.expand_path("../fixtures", __dir__))
